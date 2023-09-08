c5c9547e8afb9015c35e6dc7e12e1309ed8c0138
tranhp98 <50114999+tranhp98@users.noreply.github.com>
2023-09-08 19:37:35 -0400

    Update main.py

--- ImageClassification/Cifar10/main.py
+++ ImageClassification/Cifar10/main.py
@@ -70,27 +70,6 @@ testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=num_workers)
 
 
-# print("==> Build Model")
-# class Net(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.conv1 = nn.Conv2d(3, 32, 3)
-#         self.pool = nn.MaxPool2d(2, 2)
-#         self.conv2 = nn.Conv2d(32, 64, 3)
-#         self.conv3 = nn.Conv2d(64, 64, 3)
-#         self.fc1 = nn.Linear(64 * 4 * 4, 64)
-#         self.fc2 = nn.Linear(64, 10)
-
-#     def forward(self, x):
-#         # print(x.shape)
-#         x = self.pool(F.relu(self.conv1(x)))
-#         x = self.pool(F.relu(self.conv2(x)))
-#         x= F.relu(self.conv3(x))
-#         x = torch.flatten(x, 1) # flatten all dimensions except batch
-#         x = F.relu(self.fc1(x))
-#         x = self.fc2(x)
-#         return x
-
 print('=== Building model.. ===')
 
 # model parameters
